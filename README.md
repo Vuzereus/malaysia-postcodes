4e88a3bc1884d915ee8f2d25d2e93d22d846cd4a
Heiswayi Nrird <hnrird@gmail.com>
2024-03-03 22:04:31 +0800

    Major updates to include the latest data

--- README.md
+++ README.md
@@ -1,31 +1,36 @@
-# Malaysia Postcodes
+# Malaysia Postcodes Repository
 
-A huge list of Malaysia postcodes in a single file: [ALL-POSTCODES.csv](ALL-POSTCODES.csv)
+Welcome to the Malaysia Postcodes repository! This resource is designed to support application development by providing up-to-date information on postcodes, cities, and states in Malaysia.
 
-Postcodes are displayed in **CSV** ("Comma Separated Values") in these following formats:-
-```
-"POSTCODE","ADDRESS","CITY","STATE"
-```
+## Data Source
 
-## Individual States
+We've sourced our data from [MalaysiaPostcode](https://malaysiapostcode.com/download), specifically the following files:
 
-* [Perlis](perlis.csv) 01000 - 02800
-* [Kedah](kedah.csv) 05000 - 09810
-* [Penang / Pulau Pinang](penang.csv) 10000 - 14400
-* [Kelantan](kelantan.csv) 15000 - 18500
-* [Terengganu](terengganu.csv) 20000 - 243000
-* [Pahang](pahang.csv) 25000 - 28800 | 39000 - 39200 | 49000, 69000
-* [Perak](perak.csv) 30000 - 36810
-* [Selangor](selangor.csv) 40000 - 48300 | 63000 - 68100
-* [Kuala Lumpur](kuala_lumpur.csv) 50000 - 60000
-* [Putrajaya](putrajaya.csv) 62000 - 62988
-* [Negeri Sembilan](negeri_sembilan.csv) 70000 - 73509
-* [Melaka](melaka.csv) 75000 - 78309
-* [Johor](johor.csv) 79000 - 86900
-* [Labuan](labuan.csv) 87000 - 87033
-* [Sabah](sabah.csv) 88000 - 91309
-* [Sarawak](sarawak.csv) 93000 - 98859
+- [Malaysia_Postcode-states.csv](Malaysia_Postcode-states.csv)
+- [Malaysia_Postcode-postcodes.csv](Malaysia_Postcode-postcodes.csv)
 
----
+These files are available under the [CC BY 3.0 DEED](https://creativecommons.org/licenses/by/3.0/).
 
-Sorted by Heiswayi Nrird
+## Data Organization
+
+To make navigation easier, we've organized the data by state. Explore the individual state files for a comprehensive breakdown of postcodes.
+
+- [Johor](states/Johor.csv) (79000 - 86900)
+- [Kedah](states/Kedah.csv) (05000 - 34950)
+- [Kelantan](states/Kelantan.csv) (15000 - 18500)
+- [Melaka](states/Melaka.csv) (75000 - 78309)
+- [Negeri Sembilan](states/Negeri_Sembilan.csv) (70000 - 73509)
+- [Pahang](states/Pahang.csv) (25000 - 69000)
+- [Perak](states/Perak.csv) (30000 - 36810)
+- [Perlis](states/Perlis.csv) (01000 - 02800)
+- [Pulau Pinang](states/Pulau_Pinang.csv) (10000 - 14400)
+- [Sabah](states/Sabah.csv) (88000 - 91309)
+- [Sarawak](states/Sarawak.csv) (93000 - 98859)
+- [Selangor](states/Selangor.csv) (40000 - 68100)
+- [Terengganu](states/Terengganu.csv) (20000 - 24300)
+- [Wilayah Persekutuan Kuala Lumpur](states/Wilayah_Persekutuan_Kuala_Lumpur.csv) (50000 - 60000)
+- [Wilayah Persekutuan Labuan](states/Wilayah_Persekutuan_Labuan.csv) (87000 - 87033)
+- [Wilayah Persekutuan Putrajaya](states/Wilayah_Persekutuan_Putrajaya.csv) (62000 - 62988)
+# Licensing
+
+This repository and its data are governed by the [CC BY 3.0 DEED](https://creativecommons.org/licenses/by/3.0/). Feel free to use the information in accordance with the specified license. If you have any questions or need further assistance, don't hesitate to reach out. Happy coding!
---
+++ script.py
@@ -0,0 +1,65 @@
+import csv
+import os
+
+STATES_FILE_PATH = 'Malaysia_Postcode-states.csv'
+POSTCODES_FILE_PATH = 'Malaysia_Postcode-postcodes.csv'
+STATES_FOLDER = 'states'
+
+def extract_postcode_data(input_file: str, search_string: str, replace_string: str):
+    with open(input_file, 'r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        header = next(reader)
+
+        rows_to_save = []
+
+        for row in reader:
+            if search_string in row:
+                modified_row = [cell.replace(search_string, replace_string) for cell in row]
+                rows_to_save.append(modified_row)
+
+    if not os.path.exists(STATES_FOLDER):
+        os.makedirs(STATES_FOLDER)
+        
+    output_file = replace_string.replace(' ', '_') + '.csv'
+    output_file = os.path.join(STATES_FOLDER, output_file)
+    with open(output_file, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerows(rows_to_save)
+    print(f'File `{output_file}` has been created')
+
+def update_readme_file(states_folder_path: str):
+    content = ''
+    for filename in os.listdir(states_folder_path):
+        if filename.endswith(".csv"):
+            with open(os.path.join(states_folder_path, filename), 'r') as file:
+                lines = file.readlines()
+                first_postcode = lines[0].strip().split(',')[0]
+                last_postcode = lines[-1].strip().split(',')[0]
+                # print(f"Filename: {filename}, First Postcode: {first_postcode}, Last Postcode: {last_postcode}")
+                content += f'- [{filename.replace("_", " ").replace(".csv", "")}](states/{filename}) ({first_postcode} - {last_postcode})\n'
+    with open('README.md', 'w') as file:
+        file.write('# Malaysia Postcodes Repository\n\n')
+        file.write('Welcome to the Malaysia Postcodes repository! This resource is designed to support application development by providing up-to-date information on postcodes, cities, and states in Malaysia.\n\n')
+        file.write('## Data Source\n\n')
+        file.write('We\'ve sourced our data from [MalaysiaPostcode](https://malaysiapostcode.com/download), specifically the following files:\n\n')
+        file.write('- [Malaysia_Postcode-states.csv](Malaysia_Postcode-states.csv)\n')
+        file.write('- [Malaysia_Postcode-postcodes.csv](Malaysia_Postcode-postcodes.csv)\n\n')
+        file.write('These files are available under the [CC BY 3.0 DEED](https://creativecommons.org/licenses/by/3.0/).\n\n')
+        file.write('## Data Organization\n\n')
+        file.write('To make navigation easier, we\'ve organized the data by state. Explore the individual state files for a comprehensive breakdown of postcodes.\n\n')
+        file.write(content)
+        file.write('# Licensing\n\n')
+        file.write('This repository and its data are governed by the [CC BY 3.0 DEED](https://creativecommons.org/licenses/by/3.0/). Feel free to use the information in accordance with the specified license. If you have any questions or need further assistance, don\'t hesitate to reach out. Happy coding!')
+    print('README.md has been updated')
+
+def main():
+    with open(STATES_FILE_PATH, mode='r') as file:
+        reader = csv.reader(file)
+        data_array = []
+        for row in reader:
+            extract_postcode_data(POSTCODES_FILE_PATH, row[0], row[1])
+            data_array.append(row)
+    update_readme_file(STATES_FOLDER)
+
+if __name__ == '__main__':
+    main()
